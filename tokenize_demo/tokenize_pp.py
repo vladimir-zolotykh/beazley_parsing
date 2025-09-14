e691413f085aff0ac059c9b0454a538b8eb38973
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2025-09-14 09:13:51 +0300

    Bugfix
    -    print(pp_expr(input_str))
    +    res = expr.parse_string(str)
    +    print(res)

--- tokenize_demo/tokenize_pp.py
+++ tokenize_demo/tokenize_pp.py
@@ -1,19 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # PYTHON_ARGCOMPLETE_OK
-import pyparsing as pp
+from pyparsing import Word, nums, alphas
 
-input_str = "foo = 23 + 42 * 10"
-pp_expr = (
-    pp.Word(pp.alphas)
-    + "="
-    + pp.Word(pp.nums)
-    + "+"
-    + pp.Word(pp.nums)
-    + "*"
-    + pp.Word(pp.nums)
-)
-
+str = "foo = 23 + 42 * 10"
+expr = Word(alphas) + "=" + Word(nums) + "+" + Word(nums) + "*" + Word(nums)
 
 if __name__ == "__main__":
-    print(pp_expr(input_str))
+    res = expr.parse_string(str)
+    print(res)
