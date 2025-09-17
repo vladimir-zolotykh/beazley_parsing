fc28a41673812f6935980d24cbd273f81d959d01
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2025-09-17 20:42:27 +0300

    Add code, doctest for Sub, Div

--- expression_evaluator/evaluator.py
+++ expression_evaluator/evaluator.py
@@ -1,16 +1,24 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # PYTHON_ARGCOMPLETE_OK
-from node import Add, Number
+"""
+>>> e = Evaluator()
+>>> t1 = Add(Number(3), Number(5))
+>>> t2 = Sub(Number(10), Number(4))
+>>> t3 = Div(t2, t1)
+>>> e.visit(t3)
+0.75
+"""
+from node import Add, Sub, Div, Number
 import visitor
 
 
 class Evaluator(visitor.Visitor):
     def visit_div(self, node):
-        pass
+        return self.visit(node.left) / self.visit(node.right)
 
     def visit_sub(self, node):
-        pass
+        return self.visit(node.left) - self.visit(node.right)
 
     def visit_negate(self, node):
         return -self.visit(node.operand)
@@ -26,6 +34,6 @@ class Evaluator(visitor.Visitor):
 
 
 if __name__ == "__main__":
-    t1 = Add(Number(3), Number(5))
-    e = Evaluator()
-    print(e.visit(t1))
+    import doctest
+
+    doctest.testmod()
